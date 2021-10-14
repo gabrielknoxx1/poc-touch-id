a9d95122f0900ea2de7ccaab991b754054f99e1c
gabrielknoxx1 <byelg.sousa@gmail.com>
2021-10-14 17:21:55 -0300

    touch id feat

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import * as LocalAuthentication from "expo-local-authentication";
 
 export default function App() {
+  const [user, setUser] = useState("Não Usuario");
+  const [supported, setSupported] = useState(null);
+
+  useEffect(() => {
+    LocalAuthentication.supportedAuthenticationTypesAsync()
+      .then((sucess) => {
+        console.log("🚀 ~ file: App.js ~ line 13 ~ .then ~ sucess", sucess);
+        setSupported(true);
+      })
+      .catch((error) => {
+        console.log("🚀 ~ file: App.js ~ line 17 ~ useEffect ~ error", error);
+        setSupported(false);
+      });
+  }, []);
+
+  const handleAuth = () => {
+    LocalAuthentication.authenticateAsync({ promptMessage: "asdasd" })
+      .then((sucess) => {
+        setUser("GAbriel PDG");
+      })
+      .catch((error) => {
+        alert("Deu ruim parsa");
+      });
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+      style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}
+    >
+      <Text style={{ color: "#000" }}>{user}</Text>
+      <TouchableOpacity
+        onPress={handleAuth}
+        style={{
+          paddingHorizontal: 32,
+          paddingVertical: 16,
+          backgroundColor: "#f10666",
+          borderRadius: 5,
+        }}
+      >
+        <Text style={{ color: "#000" }}>Entrar</Text>
+      </TouchableOpacity>
+      {supported ? (
+        <Text style={{ color: "#000" }}>TouchId Habilidado</Text>
+      ) : (
+        <Text style={{ color: "#000", textAlign: "center" }}>
+          Seu Device não possui TouchId Habilitado , ao não fornece suporte ao
+          mesmo{" "}
+        </Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
